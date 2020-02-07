4a6039fb3e6ce2e8bcb2a4cb5886f9095d6e335f
Cleber Fernandes de Lima <cleberflima@hotmail.com>
2020-02-07 15:59:37 -0300

    Adicionado rotas admin - Aula #32

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const mongoose = require('mongoose');
 
+const router = require('./router');
+
 const app = express();
 
 //Configuracoes
@@ -12,6 +14,9 @@ app.use(express.json());
 //Handlebars
 app.engine('handlebars', handlebars( { defaultLayout: 'main' } ) );
 app.set('view engine', 'handlebars')
+
+//Rotas
+app.use('/admin', router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
